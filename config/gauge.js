04e247505111c150b696eba4b11b65c77b354bdb
jiaming743 <743192023@qq.com>
2019-06-05 18:22:55 +0800

    add note and some perfect

--- config/gauge.js
+++ config/gauge.js
@@ -1,43 +1,237 @@
 export const gaugeConfig = {
+  /**
+   * @description Whether to display this gauge
+   * @type {Boolean}
+   * @default show = true
+   */
   show: true,
-  center: ['50%', '50%'],
+  /**
+   * @description Radius of gauge
+   * @type {String|Number}
+   * @default radius = '60%'
+   * @example radius = '60%' | 100
+   */
   radius: '60%',
+  /**
+   * @description Center point of gauge
+   * @type {Array}
+   * @default center = ['50%','50%']
+   * @example center = ['50%','50%'] | [100, 100]
+   */
+  center: ['50%', '50%'],
+  /**
+   * @description Gauge start angle
+   * @type {Number}
+   * @default startAngle = -(Math.PI / 4) * 5
+   * @example startAngle = -Math.PI
+   */
   startAngle: -(Math.PI / 4) * 5,
+  /**
+   * @description Gauge end angle
+   * @type {Number}
+   * @default endAngle = Math.PI / 4
+   * @example endAngle = 0
+   */
   endAngle: Math.PI / 4,
+  /**
+   * @description Gauge min value
+   * @type {Number}
+   * @default min = 0
+   */
   min: 0,
+  /**
+   * @description Gauge max value
+   * @type {Number}
+   * @default max = 100
+   */
   max: 100,
+  /**
+   * @description Gauge split number
+   * @type {Number}
+   * @default splitNum = 5
+   */
   splitNum: 5,
+  /**
+   * @description Gauge arc line width
+   * @type {Number}
+   * @default arcLineWidth = 15
+   */
   arcLineWidth: 15,
-  arcItemStyle: {
+  /**
+   * @description Data item arc default style configuration
+   * @type {Object}
+   * @default dataItemStyle = {Configuration Of Class Style}
+   */
+  dataItemStyle: {
   },
-  axisLabel: {
-    show: true,
-    data: [],
-    formatter: null,
-    labelGap: 5,
-    style: {}
-  },
+  /**
+   * @description Axis tick configuration
+   * @type {Object}
+   */
   axisTick: {
+    /**
+     * @description Whether to display axis tick
+     * @type {Boolean}
+     * @default show = true
+     */
     show: true,
+    /**
+     * @description Axis tick length
+     * @type {Number}
+     * @default tickLength = 6
+     */
     tickLength: 6,
+    /**
+     * @description Axis tick default style configuration
+     * @type {Object}
+     * @default style = {Configuration Of Class Style}
+     */
     style: {
-        stroke: '#999',
-        lineWidth: 1
+      stroke: '#999',
+      lineWidth: 1
     }
   },
-  details: {
+  /**
+   * @description Axis label configuration
+   * @type {Object}
+   */
+  axisLabel: {
+    /**
+     * @description Whether to display axis label
+     * @type {Boolean}
+     * @default show = true
+     */
     show: true,
+    /**
+     * @description Axis label data (Can be calculated automatically)
+     * @type {Array}
+     * @default data = [Number...]
+     */
+    data: [],
+    /**
+     * @description Axis label formatter
+     * @type {String|Function}
+     * @default formatter = null
+     * @example formatter = '{value}%'
+     * @example formatter = (labelItem) => (labelItem.value)
+     */
     formatter: null,
-    offset: [0, 0],
-    position: 'center',
+    /**
+     * @description Axis label gap between label and axis tick
+     * @type {String|Function}
+     * @default labelGap = 5
+     */
+    labelGap: 5,
+    /**
+     * @description Axis label default style configuration
+     * @type {Object}
+     * @default style = {Configuration Of Class Style}
+     */
     style: {}
   },
-  backgroundArc: {
+  /**
+   * @description Gauge pointer configuration
+   * @type {Object}
+   */
+  pointer: {
+    /**
+     * @description Whether to display pointer
+     * @type {Boolean}
+     * @default show = true
+     */
     show: true,
+    /**
+     * @description Pointer value index of data
+     * @type {Number}
+     * @default valueIndex = 0 (pointer.value = data[0].value)
+     */
+    valueIndex: 0,
+    /**
+     * @description Pointer default style configuration
+     * @type {Object}
+     * @default style = {Configuration Of Class Style}
+     */
+    style: {
+      scale: [1, 1],
+      fill: '#f62606'
+    }
+  },
+  /**
+   * @description Data item arc detail configuration
+   * @type {Object}
+   */
+  details: {
+    /**
+     * @description Whether to display details
+     * @type {Boolean}
+     * @default show = false
+     */
+    show: false,
+    /**
+     * @description Details formatter
+     * @type {String|Function}
+     * @default formatter = null
+     * @example formatter = '{value}%'
+     * @example formatter = '{name}%'
+     * @example formatter = (dataItem) => (dataItem.value)
+     */
+    formatter: null,
+    /**
+     * @description Details position offset
+     * @type {Array}
+     * @default offset = [0, 0]
+     * @example offset = [10, 10]
+     */
+    offset: [0, 0],
+    /**
+     * @description Details position
+     * @type {String}
+     * @default position = 'center'
+     * @example position = 'start' | 'center' | 'end'
+     */
+    position: 'center',
+    /**
+     * @description Details default style configuration
+     * @type {Object}
+     * @default style = {Configuration Of Class Style}
+     */
+    style: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      textBaseline: 'middle'
+    }
+  },
+  /**
+   * @description Gauge background arc configuration
+   * @type {Object}
+   */
+  backgroundArc: {
+    /**
+     * @description Whether to display background arc
+     * @type {Boolean}
+     * @default show = true
+     */
+    show: true,
+    /**
+     * @description Background arc default style configuration
+     * @type {Object}
+     * @default style = {Configuration Of Class Style}
+     */
     style: {
         stroke: '#e0e0e0'
     }
   },
+  /**
+   * @description Gauge animation curve
+   * @type {String}
+   * @default animationCurve = 'easeOutCubic'
+   */
   animationCurve: 'easeOutCubic',
+  /**
+   * @description Gauge animation frame
+   * @type {Number}
+   * @default animationFrame = 50
+   */
   animationFrame: 50,
 }
--- config/radar.js
+++ config/radar.js
@@ -1,24 +1,94 @@
 export const radarConfig = {
+  /**
+   * @description Whether to display this radar
+   * @type {Boolean}
+   * @default show = true
+   */
   show: true,
+  /**
+   * @description Radar default style configuration
+   * @type {Object}
+   * @default style = {Configuration Of Class Style}
+   */
   radarStyle: {
     lineWidth: 1
   },
+  /**
+   * @description Radar point configuration
+   * @type {Object}
+   */
   point: {
+    /**
+     * @description Whether to display radar point
+     * @type {Boolean}
+     * @default show = true
+     */
     show: true,
+    /**
+     * @description Point radius
+     * @type {Number}
+     * @default radius = 2
+     */
     radius: 2,
+    /**
+     * @description Radar point default style configuration
+     * @type {Object}
+     * @default style = {Configuration Of Class Style}
+     */
     style: {
       fill: '#fff'
     }
   },
+  /**
+   * @description Radar label configuration
+   * @type {Object}
+   */
   label: {
+    /**
+     * @description Whether to display radar label
+     * @type {Boolean}
+     * @default show = true
+     */
     show: true,
+    /**
+     * @description Label position offset
+     * @type {Array}
+     * @default offset = [0, 0]
+     */
     offset: [0, 0],
+    /**
+     * @description Label gap between label and radar
+     * @type {Number}
+     * @default labelGap = 5
+     */
     labelGap: 5,
+    /**
+     * @description Label formatter
+     * @type {String|Function}
+     * @default formatter = null
+     * @example formatter = 'Score-{value}'
+     * @example formatter = (label) => (label)
+     */
     formatter: null,
+    /**
+     * @description Radar label default style configuration
+     * @type {Object}
+     * @default style = {Configuration Of Class Style}
+     */
     style: {
       fontSize: 10
     }
   },
+  /**
+   * @description Radar animation curve
+   * @type {String}
+   * @default animationCurve = 'easeOutCubic'
+   */
   animationCurve: 'easeOutCubic',
+  /**
+   * @description Radar animation frame
+   * @type {Number}
+   * @default animationFrame = 50
+   */
   animationFrane: 50
 }
